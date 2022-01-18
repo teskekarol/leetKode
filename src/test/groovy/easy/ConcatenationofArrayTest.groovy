e2591684a1e08a3b68521344dbd8a564debbccbb
Karol Teske <karol.teske@allegro.pl>
2022-01-18 21:16:43 +0100

    1929. Concatenation of Array

---
+++ src/test/groovy/easy/ConcatenationofArrayTest.groovy
@@ -0,0 +1,20 @@
+package easy
+
+import spock.lang.Specification
+
+class ConcatenationofArrayTest extends Specification {
+
+    def sut = new ConcatenationofArray()
+
+    def "should solve simple case"() {
+        given:
+        def input = [1,2,1] as int[]
+
+        when:
+        def result = sut.getConcatenation(input)
+
+        then:
+        result == [1,2,1,1,2,1] as int[]
+
+    }
+}
